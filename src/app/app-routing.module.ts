117f7ada7bae0bf5a96cfc0f5b46c7bb20ee5cea
Pavithra <nspavi1991@gmail.com>
2020-12-30 15:33:33 +0530

    Adding registration,login,header,footer and dashboard components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GetbooksComponent } from './components/getbooks/getbooks.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+const routes: Routes = [
+
+  {path:'',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent,
+  children:[
+    {path:'',component:GetbooksComponent}
+  ]
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
